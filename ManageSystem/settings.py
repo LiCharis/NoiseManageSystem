0ee3ff457a1908424e727fa528d18769ab3612af
LiCharis <chenggongalili@gmail.com>
2023-12-12 17:00:21 +0800

    Update settings.py

--- ManageSystem/settings.py
+++ ManageSystem/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ManageSystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ManageSystem',
+        'NAME': 'NewManageSystem',
         'USER': 'root',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
